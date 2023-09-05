4aa9e785c6ee6f604b0f38daba162b5bdb34361f
Joshua Babbitt <114113612+b0shuaj@users.noreply.github.com>
2023-09-05 18:41:48 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Edited (with love) by Joshua Babbitt.
+                automatically reload. Edited (with love) by Joshua Babbitt. Hello World!
             </p>
         </div>
     );
