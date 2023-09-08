5f05259d6e1e8953921eb7cf330d92fb15bcd02e
Joshua Babbitt <boshuaj@udel.edu>
2023-09-08 15:34:40 -0400

    solved-html-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import hank from "../src/nohohank.jpg";
 
 function App(): JSX.Element {
     return (
@@ -7,11 +9,43 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Edited (with love) by Joshua Babbitt.
-                Hello World!
-            </p>
+            <h2>Joshua Babbitt : A Really Cool Student</h2>
+
+            <ul>
+                A List of Recent Songs I Enjoy!
+                <li>Problemz - Jungle</li>
+                <li>Souk Eye - Gorillaz</li>
+                <li>Lavender Buds - MF DOOM</li>
+            </ul>
+            <img
+                src={hank}
+                width="650"
+                height="400"
+                alt="A character from Barry, NoHo Hank (my beloved)"
+            />
+            <p>&nbsp;</p>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+
+            <Container>
+                <Row>
+                    <Col>
+                        Left
+                        <p>&nbsp;</p>
+                        <svg width="100" height="50">
+                            <rect width="100" height="50" fill="red" />
+                        </svg>
+                    </Col>
+                    <Col>
+                        Right
+                        <p>&nbsp;</p>
+                        <svg width="100" height="50">
+                            <rect width="100" height="50" fill="red" />
+                        </svg>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
