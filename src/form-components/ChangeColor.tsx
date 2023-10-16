29b59980176f1d1c77a77a77fc23419d5de045b9
Joshua Babbitt <boshuaj@udel.edu>
2023-10-15 22:51:55 -0400

    solved-forms

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
 
 export function ChangeColor(): JSX.Element {
+    const colors = [
+        "red",
+        "blue",
+        "green",
+        "purple",
+        "orange",
+        "pink",
+        "teal",
+        "brown"
+    ];
+
+    const [selected, setSelected] = useState(colors[0]);
+
+    const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelected(event.target.value);
+    };
+
     return (
         <div>
             <h3>Change Color</h3>
+            <form>
+                {colors.map((color) => (
+                    <label key={color} style={{ marginRight: "10px" }}>
+                        <input
+                            type="radio"
+                            value={color}
+                            checked={selected === color}
+                            onChange={handleColorChange}
+                        />
+                        {color}
+                    </label>
+                ))}
+            </form>
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "100px",
+                    height: "100px",
+                    backgroundColor: selected,
+                    marginTop: "10px"
+                }}
+            >
+                {selected}
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +7,34 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedOption, setSelectedOption] = useState("");
+    const [result, setResult] = useState("");
+
+    useEffect(() => {
+        if (selectedOption === expectedAnswer) {
+            setResult("✔️ Correct!");
+        } else {
+            setResult("❌ Incorrect.");
+        }
+    }, [selectedOption, expectedAnswer]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <select
+                value={selectedOption}
+                onChange={(e) => setSelectedOption(e.target.value)}
+            >
+                <option value="" disabled>
+                    Select an option
+                </option>
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <div>{result}</div>
         </div>
     );
 }
